51665c490035b318e0d897937316548540760313
NNAMDI UDEH <117322301+IAM-NNAMDI@users.noreply.github.com>
2026-02-06 10:35:15 +0000

    Change default NAME and AGE in hello.sh

--- hello.sh
+++ hello.sh
@@ -5,8 +5,8 @@
 echo "Hello! 👋"
 
 # Use environment variables, or defaults if not set
-NAME=${NAME:-"Mike"}
-AGE=${AGE:-14}
+NAME=${NAME:-"Nnamdi"}
+AGE=${AGE:-20}
 
 echo "Nice to meet you, $NAME!"
 
